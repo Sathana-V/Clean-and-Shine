37b9fe77ef8f485b18b251f365503db124b12f32
Sathana-V <69391766+Sathana-V@users.noreply.github.com>
2021-09-14 23:45:46 +0530

    Update list.php

--- list.php
+++ list.php
@@ -100,7 +100,7 @@
                         <h2>ORDER LIST</h2>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="home.html">Home</a></li>
+                              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page"><a href="Bending.php">Bending</a></li>
                                 <li class="breadcrumb-item active" aria-current="page"><a href="completedservices.php">completed</a></li>
                             </ol>
